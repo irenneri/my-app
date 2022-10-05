c0ca4d5251aa5ce6df5011a72d2c7033cebdef98
Ira Bartosh <irenbarto@gmail.com>
2022-10-05 17:18:22 +0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Ira Bartosh and is
+        <a href="https://github.com/irenneri/weather-react-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
