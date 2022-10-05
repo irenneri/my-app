51063dd86100247b32938b279bf46e1637e44fd3
Ira Bartosh <irenbarto@gmail.com>
2022-10-05 17:38:02 +0300

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+
       <footer>
-        This project was coded by Ira Bartosh and is 
-        <a href="https://github.com/irenneri/weather-react-app" target="_blank">
+        This project was coded by Ira Bartosh and is{" "}
+        <a href="https://github.com/irenneri/weather-react-app">
           open-sourced on GitHub
         </a>
       </footer>
