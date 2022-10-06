2a1ddf99d4f6228c5d1fd062454c9f6277b6d7e8
Ira Bartosh <irenbarto@gmail.com>
2022-10-06 14:29:57 +0300

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Ira Bartosh and is{" "}
-        <a href="https://github.com/irenneri/weather-react-app">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Ira Bartosh and is{" "}
+          <a href="https://github.com/irenneri/weather-react-app">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
